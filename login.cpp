421f1fe8e492dc60a969877f9a14f7ab8996ea57
ehsan-ahmed-city <ehsan.ahmed@city.ac.uk>
2024-09-09 12:09:55 +0100

    Opening file for login.cpp

--- login.cpp
+++ login.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include "login.h"
+#include <string>
+#include <fstream> //to manage files
 
 using namespace std;
 
-bool logincheck(string username, string pwd) //function to check if login credentials is validd
+bool logincheck(string& username, string& pwd) //function to check if login credentials is validd
 {
-    //if(username)
+    ifstream userIndex("users.txt");
+    string existUser, existPwd; //existing usernames and password inside userindex file
+
+    if (userIndex.is_open()){
+        cout<<"Opening file...\n"; //telling user that the file is found and is opened
+        return true;
+    }
+    else{
+        cerr<<"Error when opening this file \n"; //error message if file is not found or cannot be opened
+        return false;
+    }
+
+
     
 }
