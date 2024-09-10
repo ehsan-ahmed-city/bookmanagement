39f07038485440efa65a007c95faa6f6da033b82
ehsan-ahmed-city <ehsan.ahmed@city.ac.uk>
2024-09-10 14:00:22 +0100

    login.cpp file check changes
    Checks in a while loop if the username and password entered by the user matches those in the file containing username and password information.

--- login.cpp
+++ login.cpp
@@ -12,13 +12,21 @@ bool logincheck(string& username, string& pwd) //function to check if login cred
 
     if (userIndex.is_open()){
         cout<<"Opening file...\n"; //telling user that the file is found and is opened
-        return true;
-    }
-    else{
+            while(userIndex >> existUser >> existPwd){ //loops through each username and password in file
+            if(username==existUser && pwd == existPwd){ //checks if username and password entered match those in the file
+                cout<<"Login Successful...\n";
+                return true;
+            }
+        }
+    
+    cout<<"Username and/or password is incorrect :( \n"; //no username and password match those in file
+    return false;
+    
+    }else{
         cerr<<"Error when opening this file \n"; //error message if file is not found or cannot be opened
         return false;
     }
 
-
+    
     
 }
